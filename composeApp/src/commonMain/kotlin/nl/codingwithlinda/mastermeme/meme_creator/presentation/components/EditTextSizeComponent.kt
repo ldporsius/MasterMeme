5eef7eb14cfb9959760f39909ada5335592037b7
ldporsius <linda.d.porsius@gmail.com>
2024-12-02 20:45:27 +0100

    edit text size component

--- composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/meme_creator/presentation/components/EditTextSizeComponent.kt
+++ composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/meme_creator/presentation/components/EditTextSizeComponent.kt
@@ -1,7 +1,10 @@
 package nl.codingwithlinda.mastermeme.meme_creator.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,18 +12,25 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditTextSizeComponent() {
 
-    Row {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         IconButton(onClick = { /*TODO*/ }) {
             Icon(imageVector = Icons.Default.Close, contentDescription = null)
         }
         Slider(
-            state = SliderState()
-
+            state = SliderState(),
+            modifier = Modifier.weight(1f),
         )
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Check, contentDescription = null)
+        }
     }
 }
