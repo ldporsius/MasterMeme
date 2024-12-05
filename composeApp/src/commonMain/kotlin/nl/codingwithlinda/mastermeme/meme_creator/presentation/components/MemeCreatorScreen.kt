e78e5b3fef962a7493cf9c44016f8a71381da9f1
ldporsius <linda.d.porsius@gmail.com>
2024-12-05 11:14:47 +0100

    cleanup

--- composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/meme_creator/presentation/components/MemeCreatorScreen.kt
+++ composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/meme_creator/presentation/components/MemeCreatorScreen.kt
@@ -1,46 +1,32 @@
 package nl.codingwithlinda.mastermeme.meme_creator.presentation.components
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import nl.codingwithlinda.mastermeme.meme_creator.presentation.components.save_meme.SaveMemeBottomSheet
 import nl.codingwithlinda.mastermeme.meme_creator.presentation.components.save_meme.SaveMemeOption
 import nl.codingwithlinda.mastermeme.meme_creator.presentation.state.MemeCreatorAction
 import nl.codingwithlinda.mastermeme.meme_creator.presentation.state.MemeCreatorViewState
-import nl.codingwithlinda.mastermeme.ui.theme.black
-import nl.codingwithlinda.mastermeme.ui.theme.white
-import kotlin.math.roundToInt
 
 @Composable
 fun MemeCreatorScreen(
@@ -48,38 +34,12 @@ fun MemeCreatorScreen(
     state: MemeCreatorViewState,
     onAction: (MemeCreatorAction) -> Unit,
 ) {
+    val chooseShareLauncher =
     Surface (modifier = modifier){
 
-        /* val offsetX = remember(state.selectedMemeTextIndex) {
-             mutableStateOf(0f)
-         }
-         val offsetY = remember(state.selectedMemeTextIndex) {
-             mutableStateOf(0f)
-         }*/
         var size by remember {
             mutableStateOf(Size.Zero)
         }
-        /*  val pointerInputModifier = Modifier
-              .pointerInput(Unit) {
-                  detectDragGestures(
-                      onDragEnd = {
-                          onAction(MemeCreatorAction.PositionText(
-                              parentWidth = size.width,
-                              parentHeight = size.height,
-                              offsetX = offsetX.value,
-                              offsetY = offsetY.value
-                          ))
-                      },
-                  ) { _, dragAmount ->
-                      val original = Offset(offsetX.value, offsetY. value)
-                      val summed = original + dragAmount
-                      val newValue = Offset(
-                          x = summed.x.coerceIn(-size.width/2, size.width - 100.dp.toPx()),
-                          y = summed.y.coerceIn(0f, size.height - 50.dp.toPx())                     )
-                      offsetX. value = newValue. x
-                      offsetY. value = newValue. y
-                  }
-              }*/
 
         Column {
             Spacer(modifier = Modifier.weight(1f))
@@ -103,10 +63,6 @@ fun MemeCreatorScreen(
                 state.memeImageUi.DrawImage()
 
                 state.memeTexts.onEach { memeText ->
-                    val offset = IntOffset(
-                        memeText.value.offsetX.roundToInt(),
-                        memeText.value.offsetY.roundToInt()
-                    )
                     if (memeText.value.isEditing) {
                         MemeTextComponentActive(
                             modifier = Modifier
@@ -171,7 +127,9 @@ fun MemeCreatorScreen(
                             },
                             title = "Save meme",
                             text = "Keep this meme in your local storage",
-                            onClick = {}
+                            onClick = {
+                                onAction(MemeCreatorAction.SaveMeme)
+                            }
                         )
                         SaveMemeOption(
                             icon = {
