edad4800179bd615b3c2a8d4c631cf11ac1743bc
ldporsius <linda.d.porsius@gmail.com>
2024-12-17 13:12:46 +0100

    template picker out of modal bottom sheet

--- composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/home_screen/MemeListScreen.kt
+++ composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/home_screen/MemeListScreen.kt
@@ -1,5 +1,9 @@
 package nl.codingwithlinda.mastermeme.memes_list.presentation.home_screen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -14,19 +19,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import nl.codingwithlinda.mastermeme.memes_list.presentation.components.EmptyListComponent
-import nl.codingwithlinda.mastermeme.memes_list.presentation.templates.MemeTemplatePicker
 import nl.codingwithlinda.mastermeme.memes_list.presentation.home_screen.top_bar.SortMemesTopBar
 import nl.codingwithlinda.mastermeme.memes_list.presentation.state.MemeListAction
 import nl.codingwithlinda.mastermeme.memes_list.presentation.state.MemeListViewState
+import nl.codingwithlinda.mastermeme.memes_list.presentation.templates.MemeTemplatePicker
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,10 +40,12 @@ fun MemeListScreen(
     onAction: (MemeListAction) -> Unit,
 
 ) {
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
     Scaffold(
         modifier = Modifier.fillMaxSize().safeContentPadding(),
 
-        floatingActionButton = {
+       /* floatingActionButton = {
             FloatingActionButton(onClick = {
                 onAction(MemeListAction.ShowMemePicker)
             }){
@@ -46,69 +53,98 @@ fun MemeListScreen(
             }
         },
         floatingActionButtonPosition = androidx.compose.material3.FabPosition.End,
-
+*/
         ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            SortMemesTopBar(
-                sortOptions = state.sortOptions,
-                selectedSortOption = state.selectedSortOption,
-                selectSortOption = {
-                    onAction(MemeListAction.SortMemes(it))
-                }
 
-            )
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                SortMemesTopBar(
+                    sortOptions = state.sortOptions,
+                    selectedSortOption = state.selectedSortOption,
+                    selectSortOption = {
+                        onAction(MemeListAction.SortMemes(it))
+                    }
 
-            if(state.isLoading){
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = androidx.compose.ui.Alignment.Center
-                ){
-                    CircularProgressIndicator()
-                }
-            }
-            if(state.error != null){
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = androidx.compose.ui.Alignment.Center){
-                    Text(state.error.asString())
-                }
-            }
-            if(state.memes.isEmpty()){
-                EmptyListComponent(
-                    modifier = Modifier.fillMaxSize()
                 )
-            }
-            MemeListAdaptiveLayout(
-                memes = state.sortedMemes,
-                onMemeClick = {
-                    onAction(MemeListAction.MemeClicked(it))
-                },
-                onMemeLongPress = {
-                    onAction(MemeListAction.MemeLongPressed(it))
-                }
-            )
-        }
 
-        val skipPartiallyExpanded by rememberSaveable { mutableStateOf(true) }
-        val scope = rememberCoroutineScope()
-        val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
-        if(state.showMemePicker){
-            ModalBottomSheet(
-                onDismissRequest = {
-                    onAction(MemeListAction.HideMemePicker)
-                },
-                sheetState = bottomSheetState
-            ){
-                MemeTemplatePicker(
-                    templates = state.templates,
-                    onTemplateSelected = {
-                        onAction(MemeListAction.CreateNewMeme(it))
+                if (state.isLoading) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = androidx.compose.ui.Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+                if (state.error != null) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = androidx.compose.ui.Alignment.Center
+                    ) {
+                        Text(state.error.asString())
+                    }
+                }
+                if (state.memes.isEmpty()) {
+                    EmptyListComponent(
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                MemeListAdaptiveLayout(
+                    memes = state.sortedMemes,
+                    onMemeClick = {
+                        onAction(MemeListAction.MemeClicked(it))
+                    },
+                    onMemeLongPress = {
+                        onAction(MemeListAction.MemeLongPressed(it))
                     }
                 )
             }
+
+            FloatingActionButton(
+                onClick = {
+                    onAction(MemeListAction.ShowMemePicker)
+                },
+                modifier = Modifier.padding(16.dp).align(androidx.compose.ui.Alignment.BottomEnd)
+
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
         }
+
     }
 
+    val skipPartiallyExpanded = false
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
+    AnimatedVisibility(
+        visible = state.showMemePicker,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically (
+            animationSpec = tween(durationMillis = 1000),
+            targetOffsetY = { it })
+
+    ) {
+
+            MemeTemplatePicker(
+                templates = state.templates,
+                onTemplateSelected = {
+                    onAction(MemeListAction.CreateNewMeme(it))
+                },
+                onDismiss = {
+                    onAction(MemeListAction.HideMemePicker)
+                }
+            )
+            /* ModalBottomSheet(
+            onDismissRequest = {
+                //onAction(MemeListAction.HideMemePicker)
+            },
+            sheetState = bottomSheetState,
+
+        ){
+
+        }*/
+
+    }
 }
--- composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/templates/MemePicker.kt
+++ composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/templates/MemePicker.kt
@@ -1,40 +1,90 @@
 package nl.codingwithlinda.mastermeme.memes_list.presentation.templates
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollDispatcher
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import nl.codingwithlinda.mastermeme.core.domain.model.templates.templates
 import nl.codingwithlinda.mastermeme.core.presentation.templates.MemeTemplateUi
+import nl.codingwithlinda.mastermeme.ui.theme.black
 
 @Composable
 fun MemeTemplatePicker(
     templates: List<MemeTemplateUi>,
-    onTemplateSelected: (id:String) -> Unit) {
+    onTemplateSelected: (id:String) -> Unit,
+    onDismiss: () -> Unit
+) {
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp)
-    ){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+            color = black, shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+        )
 
-        items(templates,
-            key = {
-                it.id
+    ) {
+        Box() {
+            IconButton(onClick = onDismiss) {
+                Icon(Icons.Default.Close, contentDescription = null)
             }
-        ){template ->
-            MemeTemplateItem(
-                modifier = Modifier.size(176.dp),
-                memeImageUi = template.image,
-                onClick = {
-                    onTemplateSelected(template.id)
+            Column(
+                modifier = Modifier.padding(top = 48.dp)
+            ) {
+                Row {
+                    Text("Choostemplate:")
                 }
-            )
+                Text(
+                    "Choose template for your next meme masterpiece",
+                    style = MaterialTheme.typography.bodySmall
+                )
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+
+                    items(templates,
+                        key = {
+                            it.id
+                        }
+                    ) { template ->
+                        MemeTemplateItem(
+                            modifier = Modifier.size(176.dp),
+                            memeImageUi = template.image,
+                            onClick = {
+                                onTemplateSelected(template.id)
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
