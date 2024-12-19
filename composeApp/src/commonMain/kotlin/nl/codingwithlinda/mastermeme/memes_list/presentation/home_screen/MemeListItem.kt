9e98ef9c7b64709a4068256bb64b69ed458ff790
ldporsius <linda.d.porsius@gmail.com>
2024-12-19 16:19:35 +0100

    gradient in box

--- composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/home_screen/MemeListItem.kt
+++ composeApp/src/commonMain/kotlin/nl/codingwithlinda/mastermeme/memes_list/presentation/home_screen/MemeListItem.kt
@@ -1,18 +1,24 @@
 package nl.codingwithlinda.mastermeme.memes_list.presentation.home_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.input.pointer.pointerInput
 import nl.codingwithlinda.mastermeme.core.presentation.model.MemeUi
+import nl.codingwithlinda.mastermeme.ui.theme.black
+import nl.codingwithlinda.mastermeme.ui.theme.white
 
 @Composable
 fun MemeListItem(
@@ -25,7 +31,7 @@ fun MemeListItem(
     val iconTint = if(memeUi.isFavorite){
         androidx.compose.ui.graphics.Color.Red
     }else{
-        androidx.compose.ui.graphics.Color.Unspecified
+       white
     }
     Box(
         modifier = modifier
@@ -38,12 +44,23 @@ fun MemeListItem(
             }
             .clip(shape = RoundedCornerShape(12))
         ){
-        Box(modifier = Modifier.fillMaxSize(),
+        memeUi.image.DrawThumbnail()
+        Box(modifier = Modifier
+            .matchParentSize()
+            .background(
+                brush = Brush.verticalGradient(
+                colors = listOf(
+                    androidx.compose.ui.graphics.Color.Transparent,
+                    black.copy(.5f)
+                )
+            ))
+            ,
             contentAlignment = androidx.compose.ui.Alignment.BottomEnd
-        ){
-            memeUi.image.DrawThumbnail()
-            IconButton(onClick = { onClick(memeUi.id)}) {
-                Icon(imageVector = Icons.Default.Favorite,
+        ) {
+
+            IconButton(onClick = { onClick(memeUi.id) }) {
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
                     contentDescription = null,
                     tint = iconTint
                 )
